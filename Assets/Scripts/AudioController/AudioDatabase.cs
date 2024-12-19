5b4121e2968cb07331bb7789bd6080cf8730490e
SiogunJH <homarofficial@gmail.com>
2024-12-19 14:59:44 +0100

    Update AudioDatabase.cs

--- Assets/Scripts/AudioController/AudioDatabase.cs
+++ Assets/Scripts/AudioController/AudioDatabase.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using VInspector;
+#endif
+
 [CreateAssetMenu(fileName = "NewAudioDatabase", menuName = "Audio/Audio Database")]
 public class AudioDatabase : ScriptableObject
 {
-    private Dictionary<string, AudioEntry> AudioGroups = new  Dictionary<string, AudioEntry>();
-    
+    // TODO: Display AudioGroups without the usage of SerializedDictionary or isolate it from vInspector and UnityEditor packages
+    [SerializeField] private SerializedDictionary<string, AudioEntry> AudioGroups = new();
+
     public AudioEntry? GetAudioEntry(string key)
     {
         if (AudioGroups.TryGetValue(key, out AudioEntry result))
