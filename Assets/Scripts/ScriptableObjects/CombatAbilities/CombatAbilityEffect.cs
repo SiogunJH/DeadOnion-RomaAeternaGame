b44962f007a503fad247db4d0a3ba6c91f6f68b7
AbexCorp <bartek.fryczek@onet.pl>
2024-12-27 16:34:42 +0100

    Change color switch case to dictionary

--- Assets/Scripts/ScriptableObjects/CombatAbilities/CombatAbilityEffect.cs
+++ Assets/Scripts/ScriptableObjects/CombatAbilities/CombatAbilityEffect.cs
@@ -31,52 +31,39 @@ public class CombatAbilityEffect
         CombatAbilityEffect copy = new CombatAbilityEffect();
         copy.Type = original.Type;
         copy.Amount = original.Amount;
-        foreach(var pos in original.RelativeAffectedPositions)
+        foreach (var pos in original.RelativeAffectedPositions)
         {
             copy.RelativeAffectedPositions.Add(pos);
         }
         return copy;
     }
+
+    private static Dictionary<EffectType, Color> _colors = new()
+    {
+        { EffectType.Move, new Color(0.1f, 0.3f, 0.3f) },
+        { EffectType.SkipTurn, new Color(0.2f, 0.4f, 0.2f) },
+        { EffectType.Interact, new Color(0.2f, 0.4f, 0.2f) },
+        { EffectType.Reload, new Color(0.2f, 0.4f, 0.2f) },
+        { EffectType.DamageKinetic, new Color(0.3f, 0.3f, 0.4f) },
+        { EffectType.DamageEnergy, new Color(0.1f, 0.2f, 0.4f) },
+        { EffectType.DamageFire, new Color(0.4f, 0.1f, 0.1f) },
+        { EffectType.DamagePlasma, new Color(0.3f, 0.0f, 0.4f) },
+        { EffectType.DamageAcid, new Color(0.4f, 0.3f, 0.0f) },
+        { EffectType.Heal, new Color(0.0f, 0.4f, 0.0f) },
+        { EffectType.Shield, new Color(0.0f, 0.4f, 0.4f) },
+
+        { EffectType.None, new Color(0.3f, 0.3f, 0.3f) } //Don't remove
+    };
     public static Color EffectTypeToColor(EffectType effectType)
     {
-        switch(effectType) //try to keep colors under 0.5f
+        try
         {
-            case EffectType.Move:
-                return new Color(0.1f, 0.3f, 0.3f);
-
-            case EffectType.SkipTurn:
-                return new Color(0.2f, 0.4f, 0.2f);
-
-            case EffectType.Interact:
-                return new Color(0.2f, 0.4f, 0.2f);
-
-            case EffectType.Reload:
-                return new Color(0.2f, 0.4f, 0.2f);
-
-            case EffectType.DamageKinetic:
-                return new Color(0.3f, 0.3f, 0.4f);
-
-            case EffectType.DamageEnergy:
-                return new Color(0.1f, 0.2f, 0.4f);
-
-            case EffectType.DamageFire:
-                return new Color(0.4f, 0.1f, 0.1f);
-
-            case EffectType.DamagePlasma:
-                return new Color(0.3f, 0.0f, 0.4f);
-
-            case EffectType.DamageAcid:
-                return new Color(0.4f, 0.3f, 0.0f);
-
-            case EffectType.Heal:
-                return new Color(0.0f, 0.4f, 0.0f);
-
-            case EffectType.Shield:
-                return new Color(0.0f, 0.4f, 0.4f);
-
-            case EffectType.None:
-            default:
-                return new Color(0.3f, 0.3f, 0.3f);  
+            if(_colors.ContainsKey(effectType)) return _colors[effectType];
+            else return _colors[EffectType.None];
+        }
+        catch (KeyNotFoundException)
+        {
+            return new Color(0.3f, 0.3f, 0.3f);
         }
     }
 }
