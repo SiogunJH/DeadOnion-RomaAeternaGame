47771af1fb838d1f085cec58a8e62072bb80f4b5
AbexCorp <bartek.fryczek@onet.pl>
2025-01-24 18:42:31 +0100

    Squashed commit of the following:
    commit 13cbff91da4422c3dc7fb84f30ae7c536fe1c6e6
    Author: SiogunJH <115347822+SiogunJH@users.noreply.github.com>
    Date:   Tue Jan 7 15:28:08 2025 +0100
    
        Map display and simple movement logic (#5)
    
        * Grouped TileEntities
    
        * Tile Visualization
    
        Placeholder 3D object representing a GridTile
    
        * Update SampleGridMap.asset
    
        * Transform Extensions
    
        * Update GridMap.cs
    
        Added indexer and changed GridHeight/GridWidth to Height/Width
    
        * Grid Manager
    
        * Test Scene
    
        Scene utilizaing GridManager
    
        * Enum separation
    
        * GridTile to GridTileData
    
        * GridTileController
    
        MonoBehaviour with a reference to GridTileData
    
        * MonoBehaviourSingleton
    
        * Update GridMap.cs
    
        Changed GridTile to GridTileData
    
        * OptimizeGrid function
    
        Function that removes unnecessary GridTileData instances
    
        * Pathfinding
    
        * GridEntity as Serializable
    
        * GridManager as Singleton
    
        * Grid initialization precess changes
    
        * LogGridInfo
    
        * EntityID assignment logic
    
        * TileInitialization changes
    
        * Occupant initialization changes
    
        * UserFriendlyName derived from CharacterProfile
    
        * Location default value
    
        * Namespaces update
    
        * Map property
    
        * MonoBehaviour region
    
        * Move logic
    
        * MoveDebug logic
    
        * Update SampleGridMap.asset
    
        * GridEntity update
    
        * Update Tile Visualization (Placeholder).prefab
    
        * Update TestScene_Siogun.unity
    
        * Tile occupation logic revisited
    
        GridEntities can now be marked with "Occupies the Whole Tile" flag, and will pathfind accordingly
    
        * Update GridTileData.cs
    
        Occupant-related methods now reparent occupants
    
        * Update GridEntity.cs
    
        Restricted GridEntity to be able to handle only one move request at a time

--- Assets/Scripts/Grid/GridEntity.cs
+++ Assets/Scripts/Grid/GridEntity.cs
@@ -1,15 +1,145 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using VInspector;
+#endif
+
+[System.Serializable]
 public class GridEntity : MonoBehaviour
 {
-    public string UserFriendlyName;
-    [HideInInspector] public GridTile Location;
-    public GridEntityCategory Category;
-}
+    [HideInInspector] public GridTileData Location = null;
+    [HideInInspector] public int ID = -1;
 
-public enum GridEntityCategory
-{
-    Ally, Enemy, Object
+    public string UserFriendlyName { get => CharacterProfile.Name; }
+    public GridMap Map { get => GridManager.Instance.Grid; }
+
+    public CharacterProfile CharacterProfile;
+    public GridEntityCategory Category;
+    public bool OccupiesTheWholeTile;
+
+    private bool _isMoving = false;
+
+    #region MonoBehaviour
+
+    //
+
+    #endregion
+
+    #region Moving
+
+    public Coroutine MoveTo(GridTileData targetTile)
+    {
+        // Validate
+        if (_isMoving)
+        {
+            Debug.LogWarning("Cannot move - the entity is already moving!", this);
+            return null;
+        }
+        if (!targetTile.IsEnabled)
+        {
+            Debug.LogWarning("Cannot move to a tile that is Disabled!", this);
+            return null;
+        }
+        if (OccupiesTheWholeTile && targetTile.IsOccupied)
+        {
+            Debug.LogWarning("Cannot move to a tile that is Occupied!", this);
+            return null;
+        }
+
+        // Get path
+        if (!Location.Map.FindPathBetween(this, Location, targetTile, out IEnumerable<GridTileData> path))
+        {
+            Debug.LogWarning($"Cannot find path to Tile ({targetTile.X},{targetTile.Y})!", this);
+            return null;
+        }
+
+        // Move
+        return StartCoroutine(MoveThrough(path));
+    }
+
+    private IEnumerator MoveThrough(IEnumerable<GridTileData> path)
+    {
+        // Get path
+        GridTileData[] pathArray = path.ToArray();
+
+        // Flag movement
+        _isMoving = true;
+
+        // Move through
+        for (int i = 1; i < pathArray.Length; i++) // Skip the first path tile, because it's the current tile
+        {
+            yield return StartCoroutine(StepTo(pathArray[i]));
+        }
+
+        // Unflag movement
+        _isMoving = false;
+    }
+
+    private IEnumerator StepTo(GridTileData tile)
+    {
+        // Validate
+        Debug.Assert(tile != null, "GridTileData cannot be null!", this);
+        Debug.Assert(tile.Controller != null, "GridTileController cannot be null!", this);
+
+        // Change tile
+        bool removalResult = Location.RemoveOccupant(this);
+        Debug.Assert(removalResult, $"GridEntity '{UserFriendlyName}' (ID: {ID}) could not be removed from Tile ({Location.X},{Location.Y})");
+
+        Location = tile;
+
+        bool additionResult = Location.AddOccupant(this);
+        Debug.Assert(additionResult, $"GridEntity '{UserFriendlyName}' (ID: {ID}) could not be added to Tile ({Location.X},{Location.Y})");
+
+        // Adjust position visually
+        float duration = 0.45f;
+        float elapsed = 0f;
+        Vector3 ogPos = transform.position;
+        Vector3 targetPos = tile.Controller.transform.position;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            float t = elapsed / duration; // Normalized time [0, 1]
+            t = 1f - Mathf.Pow(1f - t, 2f); // Ease-out: quadratic function
+
+            transform.position = Vector3.Lerp(ogPos, targetPos, t);
+            yield return null;
+        }
+
+        // Adjust final position
+        transform.position = targetPos;
+    }
+
+    #endregion
+
+#if UNITY_EDITOR
+
+    #region DEBUG
+
+    [Button]
+    private void MoveAtRandom()
+    {
+        // Validate
+        if (!Application.isPlaying)
+        {
+            Debug.LogWarning("MoveAtRandom can only be activated at runtime!");
+            return;
+        }
+        Debug.Assert(Location != null, "GridEntity Location cannot be null!", this);
+
+        var pool = Map.Tiles.Where(tile => tile.IsEnabled && !tile.IsOccupied && tile != Location).ToArray();
+
+        GridTileData end = pool[Random.Range(0, pool.Length)];
+
+        // Debug.Log($"Move from ({Location.X},{Location.Y}) to ({end.X},{end.Y})");
+
+        MoveTo(end);
+    }
+
+    #endregion
+
+#endif
 }
---
+++ Assets/Scripts/Grid/GridManager.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using VInspector;
+#endif
+
+public class GridManager : MonoBehaviourSingleton<GridManager>
+{
+#if UNITY_EDITOR
+    [Tab("Grid Manager/External References")]
+#endif
+
+    public GridMap Grid;
+    [SerializeField] private Transform _gridContainer;
+
+#if UNITY_EDITOR
+    [Tab("Grid Manager/Internal References")]
+#endif
+
+    [SerializeField] private GameObject _tileVisualization;
+
+#if UNITY_EDITOR
+    [EndTab]
+#endif
+
+    private const float TILE_SPACING = 1.25f;
+
+    #region MonoBehaviour
+
+    private void Start()
+    {
+        // Validate
+        Debug.Assert(Grid != null, "[Grid] is not assigned!", this);
+        Debug.Assert(_gridContainer != null, "[Grid Container] is not assigned!", this);
+
+        // Initialize Grid
+        Grid = Instantiate(Grid);
+        Grid.OptimizeGrid();
+    }
+
+    #endregion
+
+    #region Display 
+
+#if UNITY_EDITOR
+    [Button]
+#endif
+    private void DisplayGrid()
+    {
+        // Clear
+        _gridContainer.transform.RemoveChildren();
+
+        // Generate
+        for (int x = 0; x < Grid.Width; x++)
+        {
+            for (int z = 0; z < Grid.Height; z++)
+            {
+                GridTileData tile = Grid[x, z];
+
+                // Validate
+                if (!tile.IsEnabled) continue;
+
+                // Create
+                GridTileController newTile = Instantiate(_tileVisualization, _gridContainer).GetComponent<GridTileController>();
+                newTile.gameObject.name = $"Tile ({x},{z})";
+
+                newTile.Data = tile;
+                tile.Controller = newTile;
+
+                // Position
+                newTile.transform.localPosition = new(x * TILE_SPACING, 0, z * TILE_SPACING);
+
+                // Handle occupants
+                for (int i = 0; i < tile.Occupants.Count; i++)
+                {
+                    // Validate
+                    Debug.Assert(tile.Occupants[i] != null, "Occupant cannot be null!", this);
+
+                    // Create visual representation and clone
+                    tile.Occupants[i] = Instantiate(tile.Occupants[i].gameObject, newTile.transform).GetComponent<GridEntity>();
+                    tile.Occupants[i].transform.localPosition = Vector3.zero;
+
+                    // Assign data
+                    tile.Occupants[i].Location = Grid[x, z];
+                    AssignEntityID(tile.Occupants[i]);
+                }
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    [Button]
+    private void LogGridInfo()
+    {
+        string result = "";
+        foreach (var tile in Grid.Tiles) result += $"{tile}\n";
+        Debug.Log(result);
+    }
+#endif
+
+    #endregion
+
+    #region Occupant Management
+
+    private static int _lastEntityID = 0;
+
+    public static void AssignEntityID(GridEntity entity)
+    {
+        _lastEntityID++;
+        entity.ID = _lastEntityID;
+    }
+
+    #endregion
+}
