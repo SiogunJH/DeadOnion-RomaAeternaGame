77288af67c23c4e169e3479a424e825c487d89db
SiogunJH <homarofficial@gmail.com>
2025-01-25 17:21:56 +0100

    Reverted warning removal
    GridMap now validate if its dimension value was changed and if modified object is a runtime instance, before sending an assertion failure

--- Assets/Scripts/Grid/GridMap.cs
+++ Assets/Scripts/Grid/GridMap.cs
@@ -15,27 +15,47 @@ public class GridMap : ScriptableObject
     public int WidthL
     {
         get => _gridWidthL;
-        set => _gridWidthL = value;
+        set
+        {
+            if (_gridWidthL == value) return;
+            if (!EditorUtility.IsPersistent(this)) Debug.Assert(!Application.isPlaying, GRIDMAP_DIMENSIONS_EDITED_DURING_RUNTIME_WARNING);
+
+            _gridWidthL = value;
+        }
     }
 
     [SerializeField] private int _gridWidthR = 3;
     public int WidthR
     {
         get => _gridWidthR;
-        set => _gridWidthR = value;
+        set
+        {
+            if (_gridWidthR == value) return;
+            if (!EditorUtility.IsPersistent(this)) Debug.Assert(!Application.isPlaying, GRIDMAP_DIMENSIONS_EDITED_DURING_RUNTIME_WARNING);
+
+            _gridWidthR = value;
+        }
     }
 
     [SerializeField] private int _gridHeight = 3;
     public int Height
     {
         get => _gridHeight;
-        set => _gridHeight = value;
+        set
+        {
+            if (_gridHeight == value) return;
+            if (!EditorUtility.IsPersistent(this)) Debug.Assert(!Application.isPlaying, GRIDMAP_DIMENSIONS_EDITED_DURING_RUNTIME_WARNING);
+
+            _gridHeight = value;
+        }
     }
 
     [SerializeField, HideInInspector] public List<GridTileData> Tiles = new();
 
     public GridTileData this[int x, int y] { get => this[new(x, y)]; }
     public GridTileData this[Vector2 coordinates] { get => Tiles.FirstOrDefault(tile => tile.Coordinates == coordinates); }
+
+    private const string GRIDMAP_DIMENSIONS_EDITED_DURING_RUNTIME_WARNING = "Attempted to modify GridMap's dimensions during runtime. This may have unexpected results!";
 
     #region Initialization
 
