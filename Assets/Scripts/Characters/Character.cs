7c495a81bd716b6a93fe4b3b2b280fc54d00d333
AbexCorp <bartek.fryczek@onet.pl>
2025-02-01 20:15:30 +0100

    Namespace destruction

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 
 public class Character : GridEntity
--- Assets/Scripts/CombatAbilities/CombatAbilityExecutor.cs
+++ Assets/Scripts/CombatAbilities/CombatAbilityExecutor.cs
@@ -1,9 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 using UnityEngine;
-using VInspector.Libs;
 
 public class CombatAbilityExecutor : MonoBehaviourSingleton<CombatAbilityExecutor>
 {
