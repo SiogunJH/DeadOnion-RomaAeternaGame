6e8b662ba173e81823155dfb2527bd4b5af81f3c
AbexCorp <bartek.fryczek@onet.pl>
2025-02-03 00:18:34 +0100

    Move effect color to tool

--- Assets/Scripts/ScriptableObjects/CombatAbilities/CombatAbilityEffect.cs
+++ Assets/Scripts/ScriptableObjects/CombatAbilities/CombatAbilityEffect.cs
@@ -32,39 +32,11 @@ public class CombatAbilityEffect
         CombatAbilityEffect copy = new CombatAbilityEffect();
         copy.Type = original.Type;
         copy.Amount = original.Amount;
+        copy.ForTurns = original.ForTurns;
         foreach (var pos in original.RelativeAffectedPositions)
         {
             copy.RelativeAffectedPositions.Add(pos);
         }
         return copy;
-    }
-
-    private static Dictionary<EffectType, Color> _colors = new()
-    {
-        { EffectType.Move, new Color(0.1f, 0.3f, 0.3f) },
-        { EffectType.SkipTurn, new Color(0.2f, 0.4f, 0.2f) },
-        { EffectType.Interact, new Color(0.2f, 0.4f, 0.2f) },
-        { EffectType.Reload, new Color(0.2f, 0.4f, 0.2f) },
-        { EffectType.DamageKinetic, new Color(0.3f, 0.3f, 0.4f) },
-        { EffectType.DamageEnergy, new Color(0.1f, 0.2f, 0.4f) },
-        { EffectType.DamageFire, new Color(0.4f, 0.1f, 0.1f) },
-        { EffectType.DamagePlasma, new Color(0.3f, 0.0f, 0.4f) },
-        { EffectType.DamageAcid, new Color(0.4f, 0.3f, 0.0f) },
-        { EffectType.Heal, new Color(0.0f, 0.4f, 0.0f) },
-        { EffectType.Shield, new Color(0.0f, 0.4f, 0.4f) },
-
-        { EffectType.None, new Color(0.3f, 0.3f, 0.3f) } //Don't remove
-    };
-    public static Color EffectTypeToColor(EffectType effectType)
-    {
-        try
-        {
-            if(_colors.ContainsKey(effectType)) return _colors[effectType];
-            else return _colors[EffectType.None];
-        }
-        catch (KeyNotFoundException)
-        {
-            return new Color(0.3f, 0.3f, 0.3f);
-        }
     }
 }
