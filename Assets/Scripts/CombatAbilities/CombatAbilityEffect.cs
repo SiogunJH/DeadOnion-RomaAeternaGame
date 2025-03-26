118bfb065dbb09902d76c99110f995b351949c6b
SiogunJH <homarofficial@gmail.com>
2025-03-26 15:41:14 +0100

    EffectType enum redone

--- Assets/Scripts/CombatAbilities/CombatAbilityEffect.cs
+++ Assets/Scripts/CombatAbilities/CombatAbilityEffect.cs
@@ -7,35 +7,44 @@ public class CombatAbilityEffect
 {
     public EffectType Type;
     public int Amount;
+    public Vector2Int MinMax;
     public int ForAdditionalTurns; //For how many turns the effect will last, 0 is executed once, 1 is executed twice (now and next turn)
     public Character.Attribute ChangedAttribute = Character.Attribute.None;
 
     public List<Vector2Int> RelativeAffectedPositions = new();
 
+    public const int MINMAX_FIELD_FLAG = 1 << 12;
+    public const int DOT_FIELD_FLAG = 1 << 13;
+    public const int AMOUNT_FIELD_FLAG = 1 << 14;
+    public const int ATTRIBUTE_FIELD_FLAG = 1 << 15;
+
     public enum EffectType
     {
         None = 0,
+
         Move = 1,
         SkipTurn = 2,
         Interact = 3,
         Reload = 4,
-        DamageKinetic = 5,
-        DamageEnergy = 6,
-        DamageFire = 7,
-        DamagePlasma = 8,
-        DamageAcid = 9,
-        Heal = 10,
-        Shield = 11,
-        ChangeAttribute = 12
+
+        Damage = 5 + MINMAX_FIELD_FLAG,
+        Heal = 10 + MINMAX_FIELD_FLAG,
+        Shield = 11 + MINMAX_FIELD_FLAG,
+
+        // Other
+        ChangeAttribute = 12 + AMOUNT_FIELD_FLAG + ATTRIBUTE_FIELD_FLAG,
     }
 
     public static CombatAbilityEffect CloneDeep(CombatAbilityEffect original)
     {
-        CombatAbilityEffect copy = new CombatAbilityEffect();
-        copy.Type = original.Type;
-        copy.Amount = original.Amount;
-        copy.ForAdditionalTurns = original.ForAdditionalTurns;
-        copy.ChangedAttribute = original.ChangedAttribute;
+        CombatAbilityEffect copy = new()
+        {
+            Type = original.Type,
+            Amount = original.Amount,
+            MinMax = new(original.MinMax.x, original.MinMax.y),
+            ForAdditionalTurns = original.ForAdditionalTurns,
+            ChangedAttribute = original.ChangedAttribute
+        };
         foreach (var pos in original.RelativeAffectedPositions)
         {
             copy.RelativeAffectedPositions.Add(new Vector2Int(pos.x, pos.y));
