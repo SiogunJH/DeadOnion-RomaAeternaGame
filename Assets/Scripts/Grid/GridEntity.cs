0a8a094ceb7d4f0c1a67255038153975cd145f89
SiogunJH <homarofficial@gmail.com>
2025-03-26 11:58:24 +0100

    Renamed GridEntityCategory variable

--- Assets/Scripts/Grid/GridEntity.cs
+++ Assets/Scripts/Grid/GridEntity.cs
@@ -17,7 +17,7 @@ public class GridEntity : MonoBehaviour
     [Tab("Grid Entity")]
 #endif
 
-    public GridEntityCategory Category;
+    public GridEntityCategory Team;
     public bool OccupiesTheWholeTile;
 
 #if UNITY_EDITOR
