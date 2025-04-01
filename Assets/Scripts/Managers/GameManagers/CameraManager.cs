3fda31bc1740b60acb3f6067a24c03e63b2a888d
AbexCorp <bartek.fryczek@onet.pl>
2025-04-02 01:53:30 +0200

    Camera manager cinemachine 👼
    Camera manager now uses cineamchine instead of normal cam.
    New scene created with changes 💯 :>

--- Assets/Scripts/Managers/GameManagers/CameraManager.cs
+++ Assets/Scripts/Managers/GameManagers/CameraManager.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,12 +11,27 @@ public class CameraManager : MonoBehaviourSingleton<CameraManager>
 #endif
     [SerializeField]
     private Camera _cam;
-    public bool CameraIsNull => (_cam == null);
+    public bool CameraIsNull => (_defaultCamera == null);
 
+    [Header("Default")]
     [SerializeField]
-    private Transform _target;
+    private Transform _defaultTarget;
     [SerializeField]
-    private Vector3 _position;
+    private Vector3 _defaultPosition;
+
+    [Header("Animation")]
+    [SerializeField]
+    private Vector3 _animationPosition;
+    [SerializeField]
+    private Quaternion _animationTilt;
+
+
+    [Header("Cameras")]
+    [SerializeField]
+    private CinemachineVirtualCamera _defaultCamera;
+    [SerializeField]
+    private CinemachineVirtualCamera _animationCamera;
+
 #if UNITY_EDITOR
     [EndTab]
 #endif
@@ -27,8 +43,15 @@ public class CameraManager : MonoBehaviourSingleton<CameraManager>
         {
             _cam = Camera.main;
         }
-        PositionCamera(_position);
-        _position = _cam.transform.position;
+        PositionCamera(_defaultPosition);
+        _defaultPosition = _defaultCamera.transform.position;
+
+        _animationCamera.gameObject.transform.position = _animationPosition;
+        _animationCamera.gameObject.transform.rotation = _animationTilt;
+    }
+    private void Start()
+    {
+        SwitchMainCamera();
     }
 
     public void CameraLookAt(Transform target)
@@ -40,17 +63,41 @@ public class CameraManager : MonoBehaviourSingleton<CameraManager>
         }
         if (target == null) return;
 
-        _target = target;
-        _cam.transform.LookAt(target);
+        _defaultTarget = target;
+        _defaultCamera.transform.LookAt(target);
     }
 
     public void PositionCamera(Vector3 position)
     {
+        if (CameraIsNull)
+        {
+            Debug.LogWarning("Camera is not assigned");
+            return;
+        }
         if(position == null) return;
 
-        _cam.transform.position = position;
-        _position = position;
+        _defaultCamera.transform.position = position;
+        _defaultPosition = position;
 
-        CameraLookAt(_target);
+        CameraLookAt(_defaultTarget);
+    }
+
+
+    
+    
+    public void SwitchMainCamera()
+    {
+        ChangeCamera(_defaultCamera);
+    }
+    public void SwitchAnimationCamera()
+    {
+        ChangeCamera(_animationCamera);
+    }
+    private void ChangeCamera(CinemachineVirtualCamera cam)
+    {
+        _defaultCamera.Priority = 0;
+        _animationCamera.Priority = 0;
+
+        cam.Priority = 1;
     }
 }
